a6975fac6c23f39b2ff66b88ca8075969c3f0c76
Mauricio <mauricio.o.mesquita@gmail.com>
2025-02-28 00:01:29 -0500

    PR-34 fix profile page styles and forms

---
+++ src/components/dashboard/profileTable.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { LoaderCircle } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
+
+export default function ProfileTable({
+  isLoading,
+  mutation,
+  setIsLoading,
+  translate,
+  userProfileData,
+  userProfileIsLoading,
+}: {
+  isLoading: boolean;
+  mutation: any;
+  setIsLoading: (param: boolean) => void;
+  translate: any;
+  userProfileData: any;
+  userProfileIsLoading: boolean;
+}) {
+  return (
+    <>
+      <Table>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">
+              {translate["profileCard"]["firstName"]["label"]}
+            </TableCell>
+            <TableCell>
+              {userProfileIsLoading ? (
+                <Skeleton className="w-full h-6 w-32" />
+              ) : (
+                userProfileData?.first_name
+              )}
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">
+              {translate["profileCard"]["lastName"]["label"]}
+            </TableCell>
+            <TableCell>
+              {userProfileIsLoading ? (
+                <Skeleton className="h-6 w-32" />
+              ) : (
+                userProfileData?.last_name
+              )}
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">
+              {translate["profileCard"]["email"]["label"]}
+            </TableCell>
+            <TableCell>
+              {userProfileIsLoading ? (
+                <Skeleton className="h-6 w-32" />
+              ) : (
+                userProfileData?.email
+              )}
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">
+              {translate["profileCard"]["plan"]["label"]}
+            </TableCell>
+            <TableCell>
+              {userProfileIsLoading ? (
+                <Skeleton className="h-6 w-32" />
+              ) : (
+                userProfileData?.plan
+              )}
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+      <div className="mt-6">
+        <Button
+          onClick={() => {
+            if (userProfileData?.email) {
+              setIsLoading(true);
+              mutation.mutate();
+            }
+          }}
+          disabled={isLoading}
+          variant="outline"
+        >
+          {isLoading ? (
+            <div className="flex flex-row items-center italic">
+              {translate["profileCard"]["submitLoadingLabel"]}
+              <LoaderCircle className="animate-spin h-6 w-5 ml-2" />
+            </div>
+          ) : (
+            translate["profileCard"]["submitLabel"]
+          )}
+        </Button>
+      </div>
+    </>
+  );
+}
--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -13,11 +13,13 @@ import Password from "./password";
 import Text from "./text";
 
 export default function Form({
+  cancelButtonLabel,
   createAccountLinkLabel,
   fieldsToRender,
   forgotPasswordLabel,
   form,
   makeLoginLabel,
+  onCancel,
   showPasswordRules,
   submitLabel,
   submitLoadingLabel,
@@ -46,23 +48,56 @@ export default function Form({
   }
 
   return (
-    <form
-      className="space-y-4"
-      onSubmit={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        form.handleSubmit();
-      }}
-    >
-      {fieldsToRender?.map((fieldToRender: any) => (
-        <form.Field name={fieldToRender.name} key={fieldToRender.name}>
-          {(field: any) => {
-            return (
-              <div className="space-y-2">
-                {fieldToRender.type === "textArea" && (
-                  <>
-                    <Label htmlFor={field.name}>{fieldToRender.label}</Label>
-                    <Textarea
+    <>
+      <form
+        className="space-y-4"
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          form.handleSubmit();
+        }}
+      >
+        {fieldsToRender?.map((fieldToRender: any) => (
+          <form.Field name={fieldToRender.name} key={fieldToRender.name}>
+            {(field: any) => {
+              return (
+                <div className="space-y-2">
+                  {fieldToRender.type === "textArea" && (
+                    <>
+                      <Label htmlFor={field.name}>{fieldToRender.label}</Label>
+                      <Textarea
+                        field={field}
+                        fieldToRender={fieldToRender}
+                        onBlur={field.handleBlur}
+                        onChange={(e: { target: { value: string } }) => {
+                          field.handleChange(e.target.value);
+                        }}
+                      />
+                    </>
+                  )}
+                  {fieldToRender.type === "password" && (
+                    <Password
+                      field={field}
+                      fieldToRender={fieldToRender}
+                      isShowPassword={isShowPassword}
+                      onBlur={field.handleBlur}
+                      onChange={(e: any) => {
+                        field.handleChange(e.target.value);
+                        const checkRules = PasswordRulesValidation(
+                          e.target.value
+                        );
+                        setPasswordValidation((prevState: any) => ({
+                          ...prevState,
+                          ...checkRules,
+                        }));
+                      }}
+                      passwordValidation={passwordValidation}
+                      setIsShowPassword={setIsShowPassword}
+                      showPasswordRules={showPasswordRules}
+                    />
+                  )}
+                  {fieldToRender.type === "text" && (
+                    <Text
                       field={field}
                       fieldToRender={fieldToRender}
                       onBlur={field.handleBlur}
@@ -70,93 +105,72 @@ export default function Form({
                         field.handleChange(e.target.value);
                       }}
                     />
-                  </>
-                )}
-                {fieldToRender.type === "password" && (
-                  <Password
-                    field={field}
-                    fieldToRender={fieldToRender}
-                    isShowPassword={isShowPassword}
-                    onBlur={field.handleBlur}
-                    onChange={(e: any) => {
-                      field.handleChange(e.target.value);
-                      const checkRules = PasswordRulesValidation(
-                        e.target.value
-                      );
-                      setPasswordValidation((prevState: any) => ({
-                        ...prevState,
-                        ...checkRules,
-                      }));
-                    }}
-                    passwordValidation={passwordValidation}
-                    setIsShowPassword={setIsShowPassword}
-                    showPasswordRules={showPasswordRules}
-                  />
-                )}
-                {fieldToRender.type === "text" && (
-                  <Text
-                    field={field}
-                    fieldToRender={fieldToRender}
-                    onBlur={field.handleBlur}
-                    onChange={(e: { target: { value: string } }) => {
-                      field.handleChange(e.target.value);
-                    }}
-                  />
-                )}
-                {fieldToRender.type === "email" && (
-                  <Email
-                    field={field}
-                    fieldToRender={fieldToRender}
-                    onBlur={field.handleBlur}
-                    onChange={(e: { target: { value: string } }) => {
-                      field.handleChange(e.target.value);
-                    }}
-                  />
-                )}
-                <FieldInfo field={field} />
-              </div>
-            );
-          }}
-        </form.Field>
-      ))}
-      {forgotPasswordLabel && (
-        <div className="text-right">
-          <Link
-            className="text-sm text-primary hover:underline"
-            href="/recuperar-senha"
-          >
-            {forgotPasswordLabel}
-          </Link>
-        </div>
-      )}
-      <Button
-        className="w-full"
-        disabled={form.state.isSubmitting}
-        type="submit"
-      >
-        {form.state.isSubmitting ? (
-          <div className="flex flex-row items-center italic">
-            {submitLoadingLabel}
-            <LoaderCircle className="animate-spin h-5 w-5 ml-2" />
+                  )}
+                  {fieldToRender.type === "email" && (
+                    <Email
+                      field={field}
+                      fieldToRender={fieldToRender}
+                      onBlur={field.handleBlur}
+                      onChange={(e: { target: { value: string } }) => {
+                        field.handleChange(e.target.value);
+                      }}
+                    />
+                  )}
+                  <FieldInfo field={field} />
+                </div>
+              );
+            }}
+          </form.Field>
+        ))}
+        {forgotPasswordLabel && (
+          <div className="text-right">
+            <Link
+              className="text-sm text-primary hover:underline"
+              href="/recuperar-senha"
+            >
+              {forgotPasswordLabel}
+            </Link>
           </div>
-        ) : (
-          submitLabel
         )}
-      </Button>
-      {createAccountLinkLabel && (
-        <div className="text-center text-sm">
-          <Link className="text-primary hover:underline" href="/#planos">
-            {createAccountLinkLabel}
-          </Link>
-        </div>
+        <Button
+          className="w-full"
+          disabled={form.state.isSubmitting}
+          type="submit"
+        >
+          {form.state.isSubmitting ? (
+            <div className="flex flex-row items-center italic">
+              {submitLoadingLabel}
+              <LoaderCircle className="animate-spin h-5 w-5 ml-2" />
+            </div>
+          ) : (
+            submitLabel
+          )}
+        </Button>
+        {createAccountLinkLabel && (
+          <div className="text-center text-sm">
+            <Link className="text-primary hover:underline" href="/#planos">
+              {createAccountLinkLabel}
+            </Link>
+          </div>
+        )}
+        {makeLoginLabel && (
+          <div className="text-center text-sm">
+            <Link className="text-primary hover:underline" href="/login">
+              {makeLoginLabel}
+            </Link>
+          </div>
+        )}
+      </form>
+      {cancelButtonLabel && (
+        <Button
+          className="w-full mt-3"
+          disabled={form.state.isSubmitting}
+          onClick={() => onCancel()}
+          variant="outline"
+        >
+          {cancelButtonLabel}
+        </Button>
       )}
-      {makeLoginLabel && (
-        <div className="text-center text-sm">
-          <Link className="text-primary hover:underline" href="/login">
-            {makeLoginLabel}
-          </Link>
-        </div>
-      )}
-    </form>
+    </>
   );
 }
